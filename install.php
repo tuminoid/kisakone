ed466d5dbe08ab6722dc6916f9a96a46f2f92eab
Tuomo Tanskanen <tumi@tumi.fi>
2013-09-11 09:36:05 +0300

    install.php: sanitize syntax

--- install.php
+++ install.php
@@ -1,10 +1,11 @@
 <?php
 /**
- * Suomen Frisbeeliitto Kisakone
- * Copyright 2009-2010 Kisakone projektiryhmä
+ * Suomen Frisbeegolfliitto Kisakone
+ * Copyright 2009-2010 Kisakone projektiryhmÃ¤
+ * Copyright 2013 Tuomo Tanskanen <tumi@tumi.fi>
  *
  * This file performs the basic installation if Kisakone.
- * 
+ *
  * --
  *
  * This file is part of Kisakone.
@@ -23,12 +24,11 @@
 
 // Some version of PHP 5 complain about lack of time zone data, so if we can
 // we'll set it now
-if (is_callable('date_default_timezone_set')) {   
-   date_default_timezone_set("Europe/Helsinki");
+if (is_callable('date_default_timezone_set')) {
+    date_default_timezone_set("Europe/Helsinki");
 }
 
-?><!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
-            "http://www.w3.org/TR/html4/strict.dtd">
+?><!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
     <head>
         <title>Kisakone Installation</title>
@@ -36,29 +36,26 @@ if (is_callable('date_default_timezone_set')) {
     <body>
 <?php
 
-
 stripSlashesIfNecessary();
 
 if (file_exists('config.php')) {
-    
     include('config.php');
 }
 
 if (@$settings['DB_ADDRESS']) {
-
     echo "Installation appears to have been completed already. You can not
     re-enter this page without removing the configuration file.";
     return;
 }
 
 if (count($_POST)) {
-    
-    if (DoInstall()) {
+        if (DoInstall()) {
         echo "Done.";
         return;
     }
 }
 form();
+
 function form() {
 ?>
 <form method="POST">
@@ -70,15 +67,15 @@ function form() {
         <tr><td>Database name</td><td><input type="text" name="db_db"></td></tr>
         <tr><td>Table prefix</td><td><input type="text" name="db_prefix"></td></tr>
     </table>
-    
+
     <h1>Create database</h1>
     <p>Database will be created if not found with the defined prefix.</p>
     <table>
         <tr><td colspan=2"><input type="checkbox" name="db_extralogin" /> Use different login information for creating the database</td></tr>
-        
+
         <tr><td>Username</td><td><input type="text" name="db_admin_user"></td></tr>
         <tr><td>Password</td><td><input type="password" name="db_admin_pass"></td></tr>
-        
+
     </table>
     <h1>Initial Admin Account</h1>
     <table>
@@ -88,7 +85,7 @@ function form() {
     <tr><td>Last Name</td><td><input type="text" name="ad_lastname"></td></tr>
     <tr><td>E-mail</td><td><input type="text" name="ad_email"></td></tr>
     </table>
-    
+
     <h1>Miscellaneous settings</h1>
     <p><input type="checkbox" name="rewrite"> Use mod_rewrite</p>
     <p>If the web server is not apache with mod_rewrite enabled (or another compatible option), and you've enabled this setting
@@ -111,72 +108,77 @@ function DoInstall() {
     test("Database connection");
     $conn = mysql_connect($_POST['db_host'], $_POST['db_user'], $_POST['db_pass']);
     if (!$conn) {
-        return failed();        
+        return failed();
     }
     success();
-    
+
     test('Accessing database');
-    if (!mysql_select_db($_POST['db_db'])) return failed();
+    if (!mysql_select_db($_POST['db_db']))
+        return failed();
     success();
-    
+
     if (@$_POST['db_extralogin']) {
         test("Database Admin connection");
         mysql_close($conn);
         $conn = null;
         $conn = mysql_connect($_POST['db_host'], $_POST['db_admin_user'], $_POST['db_admin_pass'], true);
         if (!$conn) {
-            return failed();        
-        }        
+            return failed();
+        }
         success();
-        
+
         test('Accessing database');
-        if (!mysql_select_db($_POST['db_db'])) return failed();
+        if (!mysql_select_db($_POST['db_db']))
+            return failed();
         success();
     }
 
-    if (@$_POST['ad_user']) {    
+    if (@$_POST['ad_user']) {
         test('Admin username');
-        
+
         if (!preg_match('/[\w\d_]+/', @$_POST['ad_user'])) {
-            return failed();            
+            return failed();
         }
         success();
-            
+
     }
-    
+
     test('Config.php writable');
     $fp = @fopen('config.php', 'a');
-    if (!$fp) return failed();            
+    if (!$fp)
+        return failed();
     success();
     fclose($fp);
-    
-    
-    
+
     echo '</table><p>Starting installation</p>';
-    
+
     $prefix = @$_POST['db_prefix'];
     $q = "SELECT 1 FROM `{$prefix}RoundResult`";
     if (@mysql_query($q)) {
         echo "Database appears to be present, won't create it.<br />";
-    } else {
-        if (!InstallDB()) return;
     }
-    
+    else {
+        if (!InstallDB())
+            return;
+    }
+
     if (@$_POST['ad_user']) {
-        if (!CreateAdmin()) return;
-    } else {
+        if (!CreateAdmin())
+            return;
+    }
+    else {
         echo "Not creating an admin user, not asked to.<br>";
     }
-    
+
     SaveSettings();
-    
+
     return true;
 }
 
-
 function stripSlashesIfNecessary() {
-         if (!get_magic_quotes_gpc()) return;
-         
+         if (!get_magic_quotes_gpc())
+            return;
+
          // Luckily php doesn't mind having the array values changed while
          // in the foreach loops.
          foreach ($_POST as $k => $v) $_POST[$k] = stripslashes($v);
@@ -185,7 +187,6 @@ function stripSlashesIfNecessary() {
          foreach ($_COOKIE as $k => $v) $_COOKIE[$k] = stripslashes($v);
 
 }
-
 
 function test($n) {
     printf("<tr><td>Testing: %s</td>", $n);
@@ -205,8 +206,10 @@ function InstallDB() {
     $queries = explode(';', $source);
     $prefix = @$_POST['db_prefix'];
     foreach ($queries as $query) {
-        if (!trim($query)) continue;
-        if (trim($query) == 'SHOW WARNINGS') continue;
+        if (!trim($query))
+            continue;
+        if (trim($query) == 'SHOW WARNINGS')
+            continue;
         $query = str_replace(':', $prefix, $query);
         if (!mysql_query($query)) {
             echo $query, "<br>";
@@ -220,14 +223,15 @@ function InstallDB() {
 function CreateAdmin() {
     $prefix = @$_POST['db_prefix'];
     $query = sprintf("INSERT INTO %sUser (Username, Password, UserEmail, Role, UserFirstname, UserLastname )
-    VALUES ('%s', '%s', '%s', 'admin', '%s', '%s')",
-    $prefix,
-    mysql_real_escape_string($_POST['ad_user']),
-    mysql_real_escape_string(md5($_POST['ad_pass'])),
-    mysql_real_escape_string($_POST['ad_email']),
-    mysql_real_escape_string($_POST['ad_firstname']),
-    mysql_real_escape_string($_POST['ad_lastname']));
-    
+        VALUES ('%s', '%s', '%s', 'admin', '%s', '%s')",
+        $prefix,
+        mysql_real_escape_string($_POST['ad_user']),
+        mysql_real_escape_string(md5($_POST['ad_pass'])),
+        mysql_real_escape_string($_POST['ad_email']),
+        mysql_real_escape_string($_POST['ad_firstname']),
+        mysql_real_escape_string($_POST['ad_lastname'])
+        );
+
     if (mysql_query($query)) {
         return true;
     }
@@ -261,10 +265,11 @@ global \$settings;
 
 \$settings['USE_MOD_REWRITE'] = $rewrite;
 
-include_once('config_email.php');        
-    
+include_once('config_email.php');
+
 $q>
 EOF;
+
     $fp = fopen('config.php', 'w');
     if (!$fp) {
         return false;
