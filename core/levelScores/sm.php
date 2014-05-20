ee7793fc109bafd8de9286c2aca9f33e3f103c30
Tuomo Tanskanen <tumi@tumi.fi>
2014-05-20 13:42:05 +0300

    Fix tournament points per 2014 tour rules

--- core/levelScores/sm.php
+++ core/levelScores/sm.php
@@ -1,7 +1,8 @@
 <?php
 /**
  * Suomen Frisbeegolfliitto Kisakone
- * Copyright 2009-2010 Kisakone projektiryhm§
+ * Copyright 2009-2010 Kisakone projektiryhmä
+ * Copyright 2014 Tuomo Tanskanen <tumi@tumi.fi>
  *
  * Finnish Championship style tournament score calculation
  *
@@ -34,18 +35,26 @@ class scorecalc_level_sm
 
     function CalculateScores(&$participants, $totalHoles, $event ,$active)
     {
-        if (!count($participants)) return;
+        // Classes where 10 players only get points
+        // All other classes, 30 players get points
+        // TODO: All of this should be configurable in the UI
+        //   ie. the class names we match against are NOT constant
+        //   they just happen to be named like this in SFL Kisakone instance
+        $lowPoints = array("MPM", "MPG", "MPS", "MPL", "MPE", "FPO", "FPM", "FPG", "FPS", "FPE");
 
+        if (!count($participants))
+            return;
+
+        // By default, class is top 30 points
         $className = $participants[0]['Name'];
-        if (stripos($className, 'naiset') !== false) {
-# Janne muutti, tapani pyyti
-#            $scores = array(100,93,87,82,78,75,72,69,66,63,60,57,54,51,48,45,42,39,36,33,30,27,24,21,18,15,12,9,6,3);
-            $scores = array(100,80,65,52,40,30,20,10);
+        $scores = array(100,93,87,82,78,75,72,69,66,63,60,57,54,51,48,45,42,39,36,33,30,27,24,21,18,15,12,9,6,3);
 
-        } else {
-            $scores = array(100,93,87,82,78,75,72,69,66,63,60,57,54,51,48,45,42,39,36,33,30,27,24,21,18,15,12,9,6,3);
-# Janne muutti, tapani pyyti, vain naisill vähän pisteitä, muilla sarjoilla täydet
-#            $scores = array(100,80,65,52,40,30,20,10);
+        // If class matches one on the array above, it is top 10 points
+        foreach ($lowPoints as $low) {
+            if (substr($className, 0, 3) == $low) {
+                $scores = array(100,80,65,52,40,30,20,10);
+                break;
+            }
         }
 
         $participantCount = $active;
@@ -56,44 +65,37 @@ class scorecalc_level_sm
                 $done += $round['Completed'];
             }
 
-            /*if ($done == 0) {
+            $standing = $participant['Standing'];
+            $same = 0;
+            for ($i = $index - 1; $i >= 0; --$i) {
+                if ($participants[$i]['Standing'] == $standing) $same++;
+                else break;
+            }
+
+            for ($i = $index + 1; $i < count($participants); ++$i) {
+                if ($participants[$i]['Standing'] == $standing) $same++;
+                else break;
+            }
+
+            if ($same == 0) {
                 $score = 0;
-            } else {*/
-                $standing = $participant['Standing'];
-                $same = 0;
-                for ($i = $index - 1; $i >= 0; --$i) {
-                    if ($participants[$i]['Standing'] == $standing) $same++;
-                    else break;
+                $score = @$scores[$standing - 1];
+            } else {
+                $score = 0;
+
+                $left = $same + 1;
+                while ($left--) {
+                    $score += @$scores[$standing + $left - 1];
                 }
+                $score = round($score * 10 / ($same + 1)) / 10;
 
-                for ($i = $index + 1; $i < count($participants); ++$i) {
-                    if ($participants[$i]['Standing'] == $standing) $same++;
-                    else break;
-                }
-
-                if ($same == 0) {
-                    $score = 0;
-                    $score = @$scores[$standing - 1];
-                } else {
-                    $score = 0;
-
-                    $left = $same + 1;
-                    while ($left--) {
-                        $score += @$scores[$standing + $left - 1];
-                    }
-                    $score = round($score * 10 / ($same + 1)) / 10;
-
-                }
-
-            //}
+            }
             $score *= 10;
 
             if ($score != $participant['TournamentPoints']) {
                 $participants[$index]['TournamentPoints'] = $score;
                 $participants[$index]['Changed'] = true;
             }
-
         }
     }
-
 }
