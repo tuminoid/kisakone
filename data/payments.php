300cfce30d91aa118acafded39281233bbb23453
Tuomo Tanskanen <tuomo@tanskanen.org>
2015-02-02 22:44:42 +0200

    payments: reformatting

--- data/payments.php
+++ data/payments.php
@@ -23,94 +23,96 @@
  * */
 
 require_once 'data/db_init.php';
+require_once 'core/player.php';
 
 
 function GetFeePayments($relevantOnly = true, $search = '', $sortedBy = '', $forcePlayer = null)
 {
-   require_once 'core/player.php';
+    if ($forcePlayer)
+        $search = format_query(":Player.player_id = %d", (int) $forcePlayer);
+    else
+        $search = data_ProduceSearchConditions($search, array('FirstName', 'LastName', 'pdga', 'Username'));
 
-   if ($forcePlayer)
-      $search = format_query( ":Player.player_id = %d", (int) $forcePlayer);
-   else
-      $search = data_ProduceSearchConditions($search, array('FirstName', 'LastName', 'pdga', 'Username'));
+    $sortOrder = data_CreateSortOrder($sortedBy,
+        array('name' => array('LastName', 'FirstName'), 'LastName' => true, 'FirstName' => true, 'pdga', 'gender' => 'sex', 'Username'));
+    $year = date("Y");
 
-   $sortOrder = data_CreateSortOrder($sortedBy, array('name' => array('LastName', 'FirstName'), 'LastName' => true, 'FirstName' => true, 'pdga', 'gender' => 'sex', 'Username'));
-   $year = date("Y");
+    $membership = $relevantOnly ? "AND :MembershipPayment.Year >= $year" : "";
+    $license = $relevantOnly ? "AND :LicensePayment.Year >= $year" : "";
 
-   $query = "SELECT :User.id AS UserId, Username, Role, FirstName, LastName, Email,
-                                :Player.player_id AS PlayerId, pdga PDGANumber, sex Sex, YEAR(birthdate) YearOfBirth,
-                                :MembershipPayment.Year AS MSPYear,
-                                :LicensePayment.Year AS LPYear
-                  FROM :User
-                  INNER JOIN :Player ON :Player.player_id = :User.Player
-                  LEFT JOIN :MembershipPayment ON :MembershipPayment.Player = :Player.player_id ".($relevantOnly ? "AND :MembershipPayment.Year >= $year " : "")."
-                  LEFT JOIN :LicensePayment ON :LicensePayment.Player = :Player.player_id".($relevantOnly ? " AND :LicensePayment.Year >= $year" : "").
-                   " WHERE %s
-                   ORDER BY %s, UserId, :MembershipPayment.Year, :LicensePayment.Year";
+    $query = "SELECT :User.id AS UserId, Username, Role, FirstName, LastName, Email,
+                    :Player.player_id AS PlayerId, pdga AS PDGANumber, sex AS Sex, YEAR(birthdate) AS YearOfBirth,
+                    :MembershipPayment.Year AS MSPYear, :LicensePayment.Year AS LPYear
+                FROM :User
+                INNER JOIN :Player ON :Player.player_id = :User.Player
+                LEFT JOIN :MembershipPayment ON (:MembershipPayment.Player = :Player.player_id $membership)
+                LEFT JOIN :LicensePayment ON (:LicensePayment.Player = :Player.player_id $license)
+                WHERE $search
+                ORDER BY $sortOrder, UserId, :MembershipPayment.Year, :LicensePayment.Year";
 
-   $query = format_query($query, $search, $sortOrder);
-   $result = execute_query($query);
+    $query = format_query($query, $search, $sortOrder);
+    $result = execute_query($query);
 
-   $userid = -1;
-   $pdata = array();
-   $retValue = array();
+    $userid = - 1;
+    $pdata = array();
+    $retValue = array();
 
-   if (mysql_num_rows($result) > 0) {
-      while ($row = mysql_fetch_assoc($result)) {
-         if ($userid != $row['UserId']) {
-            if (!empty($pdata)) {
-               if (!isset($pdata['licensefees'][$year ]))
-                  $pdata['licensefees'][$year] = false;
-               if (!isset($pdata['licensefees'][$year + 1]))
-                  $pdata['licensefees'][$year + 1] = false;
+    if (mysql_num_rows($result) > 0) {
+        while ($row = mysql_fetch_assoc($result)) {
+            if ($userid != $row['UserId']) {
+                if (!empty($pdata)) {
+                    if (!isset($pdata['licensefees'][$year]))
+                        $pdata['licensefees'][$year] = false;
+                    if (!isset($pdata['licensefees'][$year + 1]))
+                        $pdata['licensefees'][$year + 1] = false;
 
-               if (!isset($pdata['membershipfees'][$year ]))
-                  $pdata['membershipfees'][$year] = false;
-               if (!isset($pdata['membershipfees'][$year + 1]))
-                  $pdata['membershipfees'][$year + 1] = false;
+                    if (!isset($pdata['membershipfees'][$year]))
+                        $pdata['membershipfees'][$year] = false;
+                    if (!isset($pdata['membershipfees'][$year + 1]))
+                        $pdata['membershipfees'][$year + 1] = false;
 
-               ksort($pdata['membershipfees']);
-               ksort($pdata['licensefees']);
+                    ksort($pdata['membershipfees']);
+                    ksort($pdata['licensefees']);
 
-               $retValue[] = $pdata;
+                    $retValue[] = $pdata;
+                }
+
+                $userid = $row['UserId'];
+                $pdata = array();
+
+                $pdata['user'] = new User($row['UserId'], $row['Username'], $row['Role'], $row['FirstName'], $row['LastName'], $row['Email'], $row['PlayerId']);
+                $pdata['player'] = new Player($row['PlayerId'], $row['PDGANumber'], $row['Sex'], $row['YearOfBirth'], $row['FirstName'], $row['LastName'], $row['Email']);
+                $pdata['licensefees'] = array();
+                $pdata['membershipfees'] = array();
             }
 
-            $userid = $row['UserId'];
-            $pdata = array();
+            if ($row['MSPYear'] != null)
+                $pdata['membershipfees'][$row['MSPYear']] = true;
 
-            $pdata['user'] = new User($row['UserId'], $row['Username'], $row['Role'], $row['FirstName'], $row['LastName'], $row['Email'], $row['PlayerId']);
-            $pdata['player'] = new Player($row['PlayerId'], $row['PDGANumber'], $row['Sex'], $row['YearOfBirth'], $row['FirstName'], $row['LastName'], $row['Email']);
-            $pdata['licensefees'] = array();
-            $pdata['membershipfees'] = array();
-         }
+            if ($row['LPYear'] != null)
+                $pdata['licensefees'][$row['LPYear']] = true;
+        }
 
-         if ($row['MSPYear'] != null)
-            $pdata['membershipfees'][$row['MSPYear']] = true;
+        if (!empty($pdata)) {
+            if (!isset($pdata['licensefees'][$year]))
+                $pdata['licensefees'][$year] = false;
+            if (!isset($pdata['licensefees'][$year + 1]))
+                $pdata['licensefees'][$year + 1] = false;
 
-         if ($row['LPYear'] != null)
-            $pdata['licensefees'][$row['LPYear']] = true;
-      }
+            if (!isset($pdata['membershipfees'][$year]))
+                $pdata['membershipfees'][$year] = false;
+            if (!isset($pdata['membershipfees'][$year + 1]))
+                $pdata['membershipfees'][$year + 1] = false;
 
-      if (!empty($pdata)) {
-         if (!isset($pdata['licensefees'][$year ]))
-            $pdata['licensefees'][$year] = false;
-         if (!isset($pdata['licensefees'][$year + 1]))
-            $pdata['licensefees'][$year + 1] = false;
+            ksort($pdata['membershipfees']);
+            ksort($pdata['licensefees']);
 
-         if (!isset($pdata['membershipfees'][$year ]))
-            $pdata['membershipfees'][$year] = false;
-         if (!isset($pdata['membershipfees'][$year + 1]))
-            $pdata['membershipfees'][$year + 1] = false;
+            $retValue[] = $pdata;
+        }
+    }
+    mysql_free_result($result);
 
-         ksort($pdata['membershipfees']);
-         ksort($pdata['licensefees']);
-
-         $retValue[] = $pdata;
-      }
-   }
-   mysql_free_result($result);
-
-   return $retValue;
+    return $retValue;
 }
 
 
@@ -119,45 +121,44 @@ function GetFeePayments($relevantOnly = true, $search = '', $sortedBy = '', $for
  */
 function MarkEventFeePayment($eventid, $participationId, $payment)
 {
-   $query = format_query("UPDATE :Participation SET EventFeePaid = FROM_UNIXTIME(%s), Approved = 1 WHERE id = %d AND Event = %d",
-                          ($payment ? time() : "NULL"), (int) $participationId, (int) $eventid);
-   $result = execute_query($query);
+    $eventid = (int) $eventid;
+    $participationId = (int) $participationId;
+    $payment = $payment ? time() : "NULL";
 
-   if (!$result)
-      return Error::Query($query);
+    $query = format_query("UPDATE :Participation
+                            SET EventFeePaid = FROM_UNIXTIME($payment), Approved = 1
+                            WHERE id = $participationId AND Event = $eventid");
+    $result = execute_query($query);
+
+    if (!$result)
+        return Error::Query($query);
 }
 
 
 function StorePayments($payments)
 {
-   foreach ($payments as $userid => $payments) {
-      $user = GetUserDetails($userid);
-      $playerid = $user->player;
+    foreach ($payments as $userid => $payments) {
+        $user = GetUserDetails($userid);
+        $playerid = $user->player;
 
-      if (isset($payments['license'])) {
-         foreach ($payments['license'] as $year => $paid) {
-            $query = format_query("DELETE FROM :LicensePayment WHERE Player = %d AND Year = %d", $playerid, $year);
-            execute_query($query);
+        if (isset($payments['license'])) {
+            foreach ($payments['license'] as $year => $paid) {
+                execute_query(format_query("DELETE FROM :LicensePayment WHERE Player = $playerid AND Year = $year"));
 
-            if ($paid) {
-               $query = format_query("INSERT INTO :LicensePayment (Player, Year) VALUES (%d, %d)", $playerid, $year);
-               execute_query($query);
+                if ($paid)
+                    execute_query(format_query("INSERT INTO :LicensePayment (Player, Year) VALUES ($playerid, $year)"));
             }
-         }
-     }
+        }
 
-     if (isset($payments['membership'])) {
-         foreach ($payments['membership'] as $year => $paid) {
-            $query = format_query("DELETE FROM :MembershipPayment WHERE Player = %d AND Year = %d", $playerid, $year);
-            execute_query($query);
+        if (isset($payments['membership'])) {
+            foreach ($payments['membership'] as $year => $paid) {
+                execute_query(format_query("DELETE FROM :MembershipPayment WHERE Player = $playerid AND Year = $year"));
 
-            if ($paid) {
-               $query = format_query("INSERT INTO :MembershipPayment (Player, Year) VALUES (%d, %d)", $playerid, $year);
-               execute_query($query);
+                if ($paid)
+                    execute_query(format_query("INSERT INTO :MembershipPayment (Player, Year) VALUES ($playerid, $year)"));
             }
-         }
-      }
-   }
+        }
+    }
 }
 
 
@@ -168,31 +169,32 @@ function StorePayments($payments)
 */
 function GetUserFees($playerid, $year)
 {
-   $query = format_query("SELECT 1 FROM :LicensePayment WHERE Player = %d AND Year = %d",
-                      $playerid, $year);
-   $result = execute_query($query);
-   $license = mysql_num_rows($result);
-   mysql_free_result($result);
+    $playerid = (int) $playerid;
+    $year = (int) $year;
 
-   $query = format_query("SELECT 1 FROM :MembershipPayment WHERE Player = %d AND Year = %d",
-                      $playerid, $year);
-   $result = execute_query($query);
-   $membership = mysql_num_rows($result);
-   mysql_free_result($result);
+    $query = format_query("SELECT 1 FROM :LicensePayment WHERE Player = $playerid AND Year = $year");
+    $result = execute_query($query);
+    $license = mysql_num_rows($result);
+    mysql_free_result($result);
 
-   return array($license, $membership);
+    $query = format_query("SELECT 1 FROM :MembershipPayment WHERE Player = $playerid AND Year = $year");
+    $result = execute_query($query);
+    $membership = mysql_num_rows($result);
+    mysql_free_result($result);
+
+    return array($license, $membership);
 }
 
 
 function EventRequiresFees($eventid)
 {
-   $query = format_query("SELECT FeesRequired FROM :Event WHERE id = %d", $eventid);
-   $result = execute_query($query);
+    $eventid = (int) $eventid;
 
-   $row = mysql_fetch_assoc($result);
-   mysql_free_result($result);
+    $query = format_query("SELECT FeesRequired FROM :Event WHERE id = $eventid");
+    $result = execute_query($query);
 
-   return $row['FeesRequired'];
+    $row = mysql_fetch_assoc($result);
+    mysql_free_result($result);
+
+    return $row['FeesRequired'];
 }
-
-
