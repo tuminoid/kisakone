622e6915882fe36263267a0afcfd086c85f0f1ed
Tuomo Tanskanen <tuomo@tanskanen.org>
2015-02-04 14:12:20 +0200

    event_management: require player.php

--- core/event_management.php
+++ core/event_management.php
@@ -2,7 +2,7 @@
 /**
  * Suomen Frisbeegolfliitto Kisakone
  * Copyright 2009-2010 Kisakone projektiryhmä
- * Copyright 2014 Tuomo Tanskanen <tuomo@tanskanen.org>
+ * Copyright 2014-2015 Tuomo Tanskanen <tuomo@tanskanen.org>
  *
  * This file defines functions used for manageing events
  *
@@ -21,6 +21,9 @@
  * You should have received a copy of the GNU General Public License
  * along with Kisakone.  If not, see <http://www.gnu.org/licenses/>.
  * */
+
+require_once 'data/player.php';
+
 
 /** ****************************************************************************
  * Function for signing up a user to an event
