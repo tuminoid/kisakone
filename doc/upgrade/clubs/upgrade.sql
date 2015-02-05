07c868c292254e648b6dce13a732e5e9cf77e5c4
Tuomo Tanskanen <tuomo@tanskanen.org>
2015-02-05 13:34:13 +0200

    clubs: upgrade

---
+++ doc/upgrade/clubs/upgrade.sql
@@ -0,0 +1,17 @@
+
+-- SQL migration from versions xxxxx and earlier
+-- Use the upgrade script upgrade.php!
+
+CREATE TABLE :Club
+(
+  id INT NOT NULL AUTO_INCREMENT,
+  Name VARCHAR(255) NOT NULL,
+  ShortName VARCHAR(20) NOT NULL,
+  INDEX(id)
+) ENGINE=InnoDB;
+
+ALTER TABLE :User ADD SflID INT AFTER PasswordChanged;
+ALTER TABLE :User ADD Club INT AFTER SflId;
+ALTER TABLE :User ADD CONSTRAINT FOREIGN KEY (Club) REFERENCES :Club(id);
+ALTER TABLE :Participation ADD Club INT AFTER TournamentPoints;
+ALTER TABLE :Participation ADD CONSTRAINT FOREIGN KEY (Club) REFERENCES :Club(id);
