b64fe396f673aa17e34a933405f240bc7444d46e
Tuomo Tanskanen <tuomo@tanskanen.org>
2015-02-14 10:40:51 +0200

    rename: drop tables created by official upgrade script

--- sfl/doc/upgrade/2015.02-rename_pdga_tables.sql
+++ sfl/doc/upgrade/2015.02-rename_pdga_tables.sql
@@ -1,1 +1,7 @@
+
+-- These are created by upgrade script and are empty
+DROP TABLE IF EXISTS kisakone_PDGAPlayers;
+DROP TABLE IF EXISTS kisakone_PDGAStats;
+
+-- Move our existing tables into kisakone namespace
 RENAME TABLE pdga_players TO kisakone_PDGAPlayers, pdga_stats TO kisakone_PDGAStats;
