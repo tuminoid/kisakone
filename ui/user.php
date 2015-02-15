43251e843f444fe95584c925518018d97fe130cb
Tuomo Tanskanen <tuomo@tanskanen.org>
2015-02-15 10:54:16 +0200

    user: fix when data is refreshed

--- ui/user.php
+++ ui/user.php
@@ -61,10 +61,10 @@ function InitializeSmartyVariables(&$smarty, $error)
             $smarty->assign('ad', $ad);
     }
 
-    if ($settings['SFL_ENABLED'] == true && ($itsme || @$_SESSION['user']->role == "admin")) {
+    if ($settings['SFL_ENABLED'] == true) {
         $fees = array('membership' => array(), 'aLicense' => array(), 'bLicense' => array());
 
-        $data = SFL_getPlayer($userid, true);
+        $data = SFL_getPlayer($userid, ($itsme || @$_SESSION['user']->role == "admin"));
         $smarty->assign('data', $data);
 
         $year = date('Y');
